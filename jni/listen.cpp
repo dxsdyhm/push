d4124a46c4467fed43ab1140f133fd2c2a6a8d08
dxsdyhm <duanxuesong12@126.com>
2018-01-03 01:52:05 -0500

    linux make success

--- jni/listen.cpp
+++ jni/listen.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
+#include <stdio.h>
 #include "listen.h"
 
 static     jclass      mClass;     // Reference to MediaPlayer class
 static     jobject     mObject;    // Weak ref to MediaPlayer Java object to call on
 static     JavaVM      *mJvm;
 static     bool  fgHasInit = false;
+static     char* classpath="push/pushinterface";
 
 int vjniListnerinit(JNIEnv* env, jclass thiz){
     jclass clazz = thiz;
@@ -26,7 +28,22 @@ void vpushinitListner(const char* initcallback)
 	    mJvm->AttachCurrentThread((void **)&env, NULL);
 	    if(env != NULL)
 	    {
-
+	        jclass pushclass = env->FindClass(classpath);
+            if(pushclass==0){
+                printf("FindClass error:%s",classpath);
+                return;
+            }
+            jmethodID listen = env->GetStaticMethodID(pushclass,"initListner","([B)V");
+            if(listen==0){
+                printf("GetMethodID error:%s",classpath);
+                return;
+            }
+            jbyte *callbackstr=(jbyte*)initcallback;
+            int len=sizeof(initcallback);
+            jbyteArray jcallarray=env->NewByteArray(len);
+            env->SetByteArrayRegion(jcallarray,0,len,callbackstr);
+            env->CallStaticVoidMethod(pushclass,listen,jcallarray);
+            printf("%s\n",initcallback);
 	    }
 	}
 	mJvm->DetachCurrentThread();
