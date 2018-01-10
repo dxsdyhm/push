1dd91f7179a73130b71063131c4d1c0901ba9b8e
USER <duanxuesong12@126.com>
2018-01-10 16:21:03 +0800

    加入配置文件测试

--- jni/push.cpp
+++ jni/push.cpp
@@ -9,7 +9,50 @@ static void initCallBack(const char* initcallback)
     vpushinitListner(initcallback);
 }
 
+inline char* getexefiledir( char* buf, int buf_len, char** ppfname = 0 )
+{
+#if defined(_WIN32)
+  GetModuleFileName(NULL,buf,buf_len);
+  char *p = strrchr(buf, '\\');
+  if ( p )
+  {
+    *p++ = 0;
+    if (ppfname)
+    {
+      char* tmp = p;
+      char *p = strrchr(tmp, '.');
+      if (p) *p = 0;
+      *ppfname = tmp;
+    }
+    return buf;
+  }
+  return NULL;
+#endif
+
+#if defined(linux)
+
+  if ( readlink("/proc/self/exe", buf, buf_len ) < 0 )
+    return NULL;
+
+  char *p = strrchr(buf, '/');
+  if (p)
+    *p = 0;
+  else
+    buf[0] = 0;
+
+  if (ppfname)
+    *ppfname = p+1;
+
+  return buf;
+#endif
+
+  return NULL;
+}
+
 int push_init(){
+   char test[1024];
+   getexefiledir(test,1024);
+   printf("getexefiledir:%s",test);
    return cpush_init(initCallBack);
 }
 
