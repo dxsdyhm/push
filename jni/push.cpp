5a07349c6b6fdfbd48b92c1b1d72c413e3f71114
USER <duanxuesong12@126.com>
2018-01-11 10:00:37 +0800

    注释测试代码

--- jni/push.cpp
+++ jni/push.cpp
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+//#include <string.h>
+//#include <unistd.h>
 
 #include "push.h"
 #include "PushInterface.h"
@@ -11,50 +11,47 @@ static void initCallBack(const char* initcallback)
     vpushinitListner(initcallback);
 }
 
-inline char* getexefiledir( char* buf, int buf_len, char** ppfname = 0 )
-{
-#if defined(_WIN32)
-  GetModuleFileName(NULL,buf,buf_len);
-  char *p = strrchr(buf, '\\');
-  if ( p )
-  {
-    *p++ = 0;
-    if (ppfname)
-    {
-      char* tmp = p;
-      char *p = strrchr(tmp, '.');
-      if (p) *p = 0;
-      *ppfname = tmp;
-    }
-    return buf;
-  }
-  return NULL;
-#endif
-
-#if defined(linux)
-
-  if ( readlink("/proc/self/exe", buf, buf_len ) < 0 )
-    return NULL;
-
-  char *p = strrchr(buf, '/');
-  if (p)
-    *p = 0;
-  else
-    buf[0] = 0;
-
-  if (ppfname)
-    *ppfname = p+1;
-
-  return buf;
-#endif
-
-  return NULL;
-}
+//inline char* getexefiledir( char* buf, int buf_len, char** ppfname = 0 )
+//{
+//#if defined(_WIN32)
+//  GetModuleFileName(NULL,buf,buf_len);
+//  char *p = strrchr(buf, '\\');
+//  if ( p )
+//  {
+//    *p++ = 0;
+//    if (ppfname)
+//    {
+//      char* tmp = p;
+//      char *p = strrchr(tmp, '.');
+//      if (p) *p = 0;
+//      *ppfname = tmp;
+//    }
+//    return buf;
+//  }
+//  return NULL;
+//#endif
+//
+//#if defined(linux)
+//
+//  if ( readlink("/proc/self/exe", buf, buf_len ) < 0 )
+//    return NULL;
+//
+//  char *p = strrchr(buf, '/');
+//  if (p)
+//    *p = 0;
+//  else
+//    buf[0] = 0;
+//
+//  if (ppfname)
+//    *ppfname = p+1;
+//
+//  return buf;
+//#endif
+//
+//  return NULL;
+//}
 
 int push_init(){
-   char test[1024];
-   getexefiledir(test,1024);
-   printf("getexefiledir:%s",test);
    return cpush_init(initCallBack);
 }
 
